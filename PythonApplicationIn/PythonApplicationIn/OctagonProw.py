179933b9e292c8a3b3356c7d0e856a6d8bddd0f8
panvk679 <panvk679@outlook.com>
2020-06-02 16:34:35 +0800

    增加八角形绘制

---
+++ PythonApplicationIn/PythonApplicationIn/OctagonProw.py
@@ -0,0 +1,6 @@
+# OctagonProw.py  八边形绘制
+import   turtle as t  # 对turtle库进行命名 ，别名 t
+t.pensize(2)          # 声明画笔大小 2     
+for i in range(8):    # 进行循环 八次
+    t.fd(100)         #画笔向前画  100 ，海龟向前进 100 forward() | fd()前进
+    t.left(45)        #画笔左转45度
---
+++ PythonApplicationIn/PythonApplicationIn/PythonDraw.py
@@ -0,0 +1,46 @@
+#PythonDraw.py Python蟒蛇绘制
+import turtle
+turtle.setup(650, 350, 200, 200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(4):
+    turtle.circle(40, 80)
+    turtle.circle(-40, 80)
+turtle.circle(40, 80/2)
+turtle.fd(40)
+turtle.circle(16, 180)
+turtle.fd(40 * 2/3)
+turtle.done()
+
+#举一反三
+#Python语法元素理解
+#- Python蟒蛇绘制共17行代码，但很多行类似
+#- 清楚理解这17行代码能够掌握Python基本绘图方法
+#- 参考框架结构、逐行分析、逐词理解
+#举一反三
+#程序参数的改变
+#- Python蟒蛇的颜色：黑色、白色、七彩色… - Python蟒蛇的长度：1节、3节、10节… - Python蟒蛇的方向：向左走、斜着走…
+#举一反三
+#计算问题的扩展
+#- Python蟒蛇绘制问题是各类图像绘制问题的代表
+#- 圆形绘制、五角星绘制、国旗绘制、机器猫绘制… - 掌握绘制一条线的方法，就可以绘制整个世界
+#
+#Python蟒蛇绘制
+#用程序绘制一条蟒蛇
+#- 貌似很有趣，可以来试试
+#- 先学会蟒蛇绘制，再绘朵玫瑰花送给
+#
+#Python蟒蛇绘制
+#设计蟒蛇的基本形状
+#
+#Python蟒蛇绘制
+#用程序绘制一条蟒蛇
+#- 问题1: 计算机绘图是什么原理？
+#一段程序为何能够产生窗体？为何能在窗体上绘制图形？
+#- 问题2: Python蟒蛇绘制从哪里开始呢？
+#如何绘制一条线？如何绘制一个弧形？如何绘制一个蟒蛇？
+
